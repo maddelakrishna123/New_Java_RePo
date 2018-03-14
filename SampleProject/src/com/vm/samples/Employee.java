6f44e35f9f0b1bfc37a17115517fd049af9d107e
valueminds123 <37062998+valueminds123@users.noreply.github.com>
2018-03-14 06:26:19 +0530

    Addiding To Git

---
+++ SampleProject/src/com/vm/samples/Employee.java
@@ -0,0 +1,19 @@
+package com.vm.samples;
+
+public class Employee {
+	int no;
+	String name;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
